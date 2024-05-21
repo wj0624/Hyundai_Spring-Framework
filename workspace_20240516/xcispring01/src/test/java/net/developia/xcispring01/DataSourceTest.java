d06220f12072ed8b426036a8b2ddf4a3506439c7
wj0624 <wonjeong4538@gmail.com>
2024-05-21 17:36:19 +0900

    240521

--- workspace_20240516/xcispring01/src/test/java/net/developia/xcispring01/DataSourceTest.java
+++ workspace_20240516/xcispring01/src/test/java/net/developia/xcispring01/DataSourceTest.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +24,29 @@ import lombok.extern.log4j.Log4j;
 public class DataSourceTest {
 	@Autowired
 	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMyBatis() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+			Connection con = session.getConnection()) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 
 	@Test
 	public void testConnection() {
 		try(Connection con = dataSource.getConnection()) {
+			log.debug(con);
 			log.info(con);
+			log.warn(con);
+			log.error(con);
+			log.fatal(con);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
