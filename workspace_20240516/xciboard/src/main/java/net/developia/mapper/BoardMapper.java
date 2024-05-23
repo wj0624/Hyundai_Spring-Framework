f2310bf27a85cbb1c365c883ae03e65b52138ba6
wj0624 <wonjeong4538@gmail.com>
2024-05-23 18:08:21 +0900

    240523

--- workspace_20240516/xciboard/src/main/java/net/developia/mapper/BoardMapper.java
+++ workspace_20240516/xciboard/src/main/java/net/developia/mapper/BoardMapper.java
@@ -8,6 +8,18 @@ public interface BoardMapper {
 //	@Select("select bno, title, content, writer, regdate, updatedate from tbl_board where bno > 0")
 	public List<BoardVO> getList();
 	
+	// create
 	public void insert(BoardVO board);
 	public void insertSelectkey(BoardVO board);
+	
+	// read
+	public BoardVO read(Long bno);
+	
+	// update
+	public int update(BoardVO board);
+	
+	// delete
+	public int delete(Long bno);
+
+	
 }
--- workspace_20240516/xciboard/src/test/java/net/developia/persistence/BoardMapperTests.java
+++ workspace_20240516/xciboard/src/test/java/net/developia/persistence/BoardMapperTests.java
@@ -48,5 +48,27 @@ public class BoardMapperTests {
 		
 		log.info(board);
 	}
-
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT: "+mapper.delete(3L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: "+count);
+	}
 }
