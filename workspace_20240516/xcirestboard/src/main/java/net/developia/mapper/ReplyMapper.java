f684523c11acd67e6d0fa7221e1f23292974ee95
wj0624 <wonjeong4538@gmail.com>
2024-05-28 09:58:07 +0900

    240527

---
+++ workspace_20240516/xcirestboard/src/main/java/net/developia/mapper/ReplyMapper.java
@@ -0,0 +1,27 @@
+package net.developia.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import net.developia.domain.Criteria;
+import net.developia.domain.ReplyVO;
+
+public interface ReplyMapper {
+	// 등록
+	public int insert(ReplyVO vo);
+	
+	// 조회
+	public ReplyVO read(Long rno);
+	
+	// 삭제
+	public int delete(Long rno);
+	
+	// 수정
+	public int update(ReplyVO reply);
+	
+	// 댓글 목록 및 페이징 처리
+	public List<ReplyVO> getListWithPaging(
+			@Param("cri") Criteria cri, 
+			@Param("bno") Long bno);
+}
