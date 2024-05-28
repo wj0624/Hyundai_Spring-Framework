cc6d68b72968925da00598819ec39b137164f5a4
wj0624 <wonjeong4538@gmail.com>
2024-05-28 09:40:11 +0900

    240528

--- workspace_20240516/.metadata/.plugins/org.eclipse.core.resources/.history/a1/80321aed7016001f131a9ca5446557c6
+++
@@ -1,5 +0,0 @@
-package net.developia.spring01.di301e;
-
-public interface FileOutputter {
-	public void output(String msg) throws Exception;
-}
---
+++ workspace_20240516/xcirestboard/src/main/java/net/developia/controller/ReplyController.java
@@ -0,0 +1,84 @@
+package net.developia.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import net.developia.domain.Criteria;
+import net.developia.domain.ReplyVO;
+import net.developia.service.ReplyService;
+
+@RequestMapping("/replies")
+@RestController
+@Log
+@AllArgsConstructor
+public class ReplyController {
+	
+	private ReplyService service;
+	
+	@PostMapping(value="/new",
+			consumes="application/json",
+			produces= {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo){
+		log.info("ReplyVO 등록 : "+vo);
+		int insertCount = service.register(vo);
+		log.info("Reply INSERT COUNT : "+insertCount);
+		return insertCount==1 ? 
+				new ResponseEntity<>("success", HttpStatus.OK) :
+				new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@GetMapping(value="/pages/{bno}/{page}",
+				produces= {MediaType.APPLICATION_XML_VALUE,
+							MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno){
+		log.info("Get List........");
+		Criteria cri = new Criteria(page, 10);
+		log.info("Criteria : "+ cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{rno}",
+				produces = {MediaType.APPLICATION_XML_VALUE,
+						    MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		log.info("get : "+rno);
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value="/{rno}",
+				produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		log.info("remove : "+rno);
+		return service.remove(rno)==1 ? 
+				new ResponseEntity<>("success", HttpStatus.OK) :
+				new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH},
+					value ="/{rno}",
+					consumes = "application/json",
+					produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno){
+		log.info("ReplyVO 수정 : "+vo);
+		vo.setRno(rno);
+		log.info("rno : "+rno);
+		
+		return service.modify(vo)==1 ? 
+				new ResponseEntity<>("success", HttpStatus.OK) :
+				new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
